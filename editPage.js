46670529ae7ea0e78bd0eda018bf6a2e8775746a
PhilPsych <32993142+PhilPsych@users.noreply.github.com>
2017-10-25 11:16:04 -0400

    Added initial files.
    Added HTML (up to Angner, 2010), CSS, and JS files. The noted difference is the addition of an edit feature that allows users to edit the client-facing website, then download these edits to a file for publishing to the repository.

---
+++ editPage.js
@@ -0,0 +1,40 @@
+var editable = document.getElementsByClassName("content");
+
+function dateAndEdits () {
+    if (localStorage.getItem("userEdits") === null) {
+        null;
+    }
+    else {
+        document.getElementById("material").innerHTML = localStorage.getItem("userEdits");
+    }
+    var changeDate = document.lastModified;
+    document.getElementById("lastup").innerHTML = ("Last Updated On " + changeDate);
+}
+
+function editButton () {
+    if (editable[0].isContentEditable == false) {
+        for (i = 0; i < editable.length; i++) {
+            editable[i].contentEditable = true;
+        }
+        document.getElementById("editToggle").innerHTML = "Save Edits";
+    }
+    else {
+        for (i = 0; i < editable.length; i++) {
+            editable[i].contentEditable = false;
+        }
+        saveEdits ();
+        document.getElementById("editToggle").innerHTML = "Edit";
+    }
+}
+
+function saveEdits () {
+    var editElem = document.getElementById("material");
+    var userVersion = editElem.innerHTML;
+    localStorage.userEdits = userVersion;
+}
+
+function download() {
+    var printOut = String(document.getElementById("material").innerHTML);
+    uriContent = "data:application/octet-stream," + encodeURIComponent(printOut);
+    newWindow = window.open(uriContent, "neuesDokument");
+}
